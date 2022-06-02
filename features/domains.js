3b7cbaf5c12f853dfc9f7c529adf982cdb0cfffd
themirrazz <89490659+themirrazz@users.noreply.github.com>
2022-06-02 03:11:03 -0700

    Create domains.js

---
+++ features/domains.js
@@ -0,0 +1,11 @@
+[
+  ".com",".org",".net",".gov",".edu",".app",".site",".dev",".io",".co",
+  ".chrome", ".ie", ".fox", ".fo", ".you", ".me", ".fun", ".sex", ".us",
+  ".uk", ".rs", ".th", ".dog", ".horse",".website",".abc",".xyz",
+  ".ga",".gb",".gd",".gf",".go",".ms",".gle",".google",".tv",".news",".shop",
+  ".store",".online",".network",".pe",".beer",".kn",".onion",".ca",".cafe",
+  ".br",".ht",".pa",".pl",".ws",".ge",".mo",".mov",".movie",".moe",".do",".chat",
+  ".at",".cat",".wtf",".in",".si",".gh",".ppp",".sn",".no",".es",".am",
+  ".bo",".boo",".book",".yt",".tube",".ch",".gg",".help",".ke",
+  ".mu",".to",
+]
